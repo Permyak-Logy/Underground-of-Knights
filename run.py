63769500897fce1068115fb6152213de32193959
Permyak-Logy <maxsika2004@mail.ru>
2019-12-12 19:14:06 +0400

    Меню, Главный цикл, Кнопки. v1.1
    Создана первая версия меню с некоторыми кнопками.

--- run.py
+++ run.py
@@ -12,35 +12,52 @@ class GameExample:
     '''
 
     def __init__(self):
+        '''Инициализация'''
         pygame.init()
 
+        # Скрытие курсора
+        pygame.mouse.set_visible(False)
+
         # Инициализация параметров окна
-        self.size = self.width, self.height = 500, 500
+        self.size = self.width, self.height = 800, 500
 
         # Инициализация главного кадра игры
         self.main_screen = pygame.display.set_mode(self.size)
 
-        # Загрузка кадра меню и его виджетов
-        self.load_menu_screen()
+        # Загрузка меню
+        self.load_menu()
 
         # Некоторые переменныне в игре
-        self.mode = MODE_MENU  # Переключатель между режимом меню (0) / в игре (1) / в паузе (2)
+        self.mode = MODE_MENU  # Переключатель между режимом меню MODE_MENU / в игре MODE_GAME / в паузе MODE_PAUSE
         self.battle_status = False  # Переключатель между режимом битвы (True) и мира (False)
-        self.running = False
+        self.running = False  # Активнойсть программы
+        self.image_arrow = pygame.transform.scale(self.load_image('arrow.png', -1), (22, 22))  # Картинка курсора
 
         # Центрирование окна
         self.center()
 
-    def load_menu_screen(self):
-        self.menu_screen = pygame.Surface([self.width, self.height])  # Холст меню
-        self.menu_buttons = []  # Список виджетов меню
-        
-        btn_test = PushButton(self.menu_screen, size=(100, 50), color=(0, 255, 255), pos=(10, 30),
-                              func=self.some_func)  # Тестовая кнопка
-        self.menu_buttons.append(btn_test)
+    def load_menu(self):
+        '''Загруска меню'''
 
-    def load_game_screen(self):
-        self.play_screen = pygame.Surface([self.width, self.height])  # Холст игры
+        # Временное сокращение некоторых функций
+        _Font = pygame.font.Font
+        _Color = pygame.Color
+
+        # Пункты меню
+        puncts = [PushButton(text='Играть', pos=(10, 400), size=(150, 50), color=_Color('green'),
+                             font=_Font(None, 30), func=lambda: print('Играть')),
+
+                  PushButton(text='Настройки', pos=(210, 400), size=(150, 50), color=_Color('yellow'),
+                             font=_Font(None, 30), func=lambda: print('Настройки')),
+
+                  PushButton(text='Руководство', pos=(410, 400), size=(150, 50), color=_Color('#942ad4'),
+                             font=_Font(None, 30), func=lambda: print('Руководство')),
+
+                  PushButton(text='Выйти', pos=(610, 400), size=(150, 50), color=_Color('red'),
+                             font=_Font(None, 30), func=lambda: print('Выход'))]
+
+        # Создание меню с раннее созданными пунктами
+        self.menu = Menu(self, puncts)
 
     def mainloop(self):
         ''' Главный цикл программы '''
@@ -56,36 +73,32 @@ class GameExample:
                 if event.type == pygame.KEYDOWN:
                     self.keyPressEvent(event)
 
-            if self.mode == MODE_PLAY:
-                self.checkEvents()
-
             # Отрисовка экрана
             self.main_screen.fill(pygame.Color('black'))
             if self.mode == MODE_MENU:
-                self.main_screen.blit(self.menu_screen, (0, 0))
+                # Рисование меню
+                self.menu.render(self.main_screen)
             if self.mode == MODE_PLAY:
-                self.main_screen.blit(self.play_screen, (0, 0))
+                pass
+            if pygame.mouse.get_focused():
+                # Отрисовка курсора
+                self.main_screen.blit(self.image_arrow, (pygame.mouse.get_pos()))
 
-            # Обновление кадра
+            # Обновление дисплея
             pygame.display.flip()
 
         # Закрытие игры
         self.close()
 
-    def some_func(self):
-        '''Функция теста функционала кнопки.'''
-        print('Some func был активирован')
-
-    def checkEvents(self):
-        pass
-
     def mousePressEvent(self, event):
+        '''События мыши'''
         if self.mode == MODE_MENU:
-            click_pos = event.pos
-            for buttons in self.menu_buttons:
-                buttons.check_click(click_pos)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # Проверяет элементы после нажатия мышкой
+                self.menu.checkOnPress(event.pos)
 
     def keyPressEvent(self, event):
+        '''События клавиатуры'''
         pass
 
     def load_image(self, name, colorkey=None):
@@ -112,33 +125,76 @@ class GameExample:
         # self.move(qp.topLeft())
 
     def close(self):
+        '''Выход из игры'''
         pygame.quit()
         print('-----Game closed------')
+
+
+class Menu:
+    def __init__(self, game, punkts=[]):
+        self.game = game  # Подключение игры к меню
+        background = self.game.load_image('background menu.jpg')  # Загрузка картинки фона
+        self.image_background = pygame.transform.scale(background, self.game.size)  # Преобразование фона
+        self.punkts = punkts  # Занесение пунктов меню
+
+    def checkOnPress(self, pos):
+        '''Проверяет меню после клика мышки''' # Не могу придумать...
+        for punckt in self.punkts:
+            punckt.on_click(pos)
+
+    def render(self, screen, num_punkt=-1):
+        '''Рисует меню'''
+        screen.blit(self.image_background, (0, 0))  # Накладывет фон
+        for punkt in self.punkts:
+            # Рисует все пункты меню
+            punkt.draw(screen, ispressed=(punkt.number == num_punkt))
 
 
 class PushButton:
     '''
     Кнопки в меню игры
     '''
-    def __init__(self, screen, size=(30, 10), pos=(0, 0), color=(255, 255, 255), image=None, func=None):
-        self.screen = screen
-        self.pos = self.x, self.y = pos
-        self.size = self.width, self.height = size
-        self.color = color
-        self.func = func
-        self.surface = pygame.Surface(list(size))
-        self.surface.fill(color)
-        if image is not None:
-            self.surface.blit(pygame.transform.scale(image, size))
-        self.screen.blit(self.surface, pos)
+    def __init__(self, text=None, pos=(0, 0), size=(30, 10), font=None, color=(255, 255, 255),
+                 color_active=(0, 255, 255), color_text=(0, 0, 0), image=None, func=None, number=0):
+        '''Инициализация'''
+        self.text = text  # Текст
+        self.font = (pygame.font.Font(None, size[0]) if font is None else font)  # Шрифт
+        self.pos = self.x, self.y = pos  # Позиция
+        self.size = self.width, self.height = size  # Размеры
+        self.number = number  # Порядковый номер пункта
+        # Цвет основной / активный / текста и картинка
+        self.color, self.color_active, self.color_text = color, color_active, color_text
+        self.func = func  # Функция кнопки
+        # Картинка
+        self.image = pygame.transform.scale(image, self.size) if image is not None else None
 
-    def check_click(self, pos):
-        '''Вызывает функцию подключённую к кнопке если она была нажата'''
+    def draw(self, screen, ispressed=False):
+        '''Рисует кнопку на screen'''
+        surface = pygame.Surface(self.size)
+        surface.fill(self.color if not ispressed else self.color_active)
+        if self.image is not None:
+            surface.blit(self.image, self.pos)
+        if self.text is not None:
+            text = self.font.render(self.text, 1, self.color_text)
+            text_x = self.width // 2 - text.get_width() // 2
+            text_y = self.height // 2 - text.get_height() // 2
+            surface.blit(text, (text_x, text_y))
+
+        screen.blit(surface, self.pos)
+
+    def get_focused(self, pos):
+        '''Возвращает True если pos  находится на кнопке, иначе False'''
+        if not self.x <= pos[0] <= self.x + self.width:
+            return False
+        if not self.y <= pos[1] <= self.y + self.height:
+            return False
+        return True
+
+    def on_click(self, pos):
+        '''Вызывает функцию подключённую к кнопке, если она была нажата'''
         if self.func is None:
             return
-        if not self.x <= pos[0] <= self.x + self.width:
-            return
-        if not self.y <= pos[1] <= self.y + self.height:
+        if not self.get_focused(pos):
             return
         self.func()
 
